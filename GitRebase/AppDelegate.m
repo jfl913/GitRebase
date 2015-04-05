148c40141246c68ea7de8c2ce6d5285fd4e1d5dc
jfl913 <jfl913@163.com>
2015-04-05 22:01:10 +0800

    add master1

--- GitRebase/AppDelegate.m
+++ GitRebase/AppDelegate.m
@@ -18,6 +18,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"master0");
+    NSLog(@"master1");
+
     return YES;
 }
 
